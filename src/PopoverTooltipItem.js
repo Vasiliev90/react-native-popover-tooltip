eabb7161b1c6eac75c46038023dec5e31c534c57
MikhailVasiliev <mr.m.vasiliev@gmail.com>
2018-06-09 02:16:54 +0300

    increased tappable area

--- src/PopoverTooltipItem.js
+++ src/PopoverTooltipItem.js
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: 10
+    padding: 15
   },
   selectedIcon: {
     resizeMode: 'contain',
